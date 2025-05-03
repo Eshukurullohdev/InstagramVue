9200dbeec6bd48ea9d9096ca0d12e7e9f3c762f5
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-05-03 13:33:05 +0500

    asj

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,9 @@
 <template>
-<<<<<<< HEAD
 <Menu/>
         <div class="absolute top-0 left-0 w-full h-full bg-[#7F4DFE] opacity-50">
             <div class="container_large w-[414px] h-[482px] bg-white rounded-[20px] p-[50px] relative z-999 mt-[170px]">
                 <RouterLink to="/">
                     <i class="absolute top-[20px] right-[20px] text-[20px] cursor-pointer hover:bg-[#f0f2f5] p-3  rounded-full fa-solid fa-xmark"></i>
-=======
-<Menu />
-        <div class="absolute top-0 left-0 w-full h-full bg-[#7F4DFE] opacity-50">
-            <div class="container_large w-[414px] h-[482px] bg-white rounded-[20px] p-[50px] relative z-999 mt-[170px]">
-                <RouterLink to="/">
-                    <i class="absolute top-[20px] right-[20px] text-[20px]  cursor-pointer hover:bg-[#f0f2f8] p-3 rounded-full fa-solid fa-xmark"></i>
->>>>>>> faaea80412493a8619787eaea267413581c437c8
                 </RouterLink>
                 <h1 class="text-[24px] text-black font-semibold">Telefon raqamini Kiriting</h1>
                 <h2 class="mt-[20px]">Tasdiqlash kodini SMS orqali yuboramiz</h2>
@@ -24,12 +16,6 @@
             </div>
         </div>
 </template>
-<<<<<<< HEAD
 <script setup>
 import Menu from'./Menu.vue';
-=======
-
-<script setup>
-import Menu from './Menu.vue';
->>>>>>> faaea80412493a8619787eaea267413581c437c8
 </script>
